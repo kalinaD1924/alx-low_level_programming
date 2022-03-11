ac1d9e353900da8cf9abb827510485975889c8f8
kalinaD1924 <kaliyyeek@gmail.com>
2022-03-11 16:04:47 +0100

    C

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
-{
 	// This program will create same sequence of
 	// random numbers on every program run
+int main(void)
+{
 
 	for(int i = 0; i<5; i++)
 		printf(" %d ", rand());
