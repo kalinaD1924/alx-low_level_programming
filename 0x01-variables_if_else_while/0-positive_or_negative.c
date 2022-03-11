e7f83c08c8a5e468dcd6a029ce192d66f7671e82
kalinaD1924 <kaliyyeek@gmail.com>
2022-03-11 16:10:16 +0100

    C

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
 
-	// This program will create same sequence of
-	// random numbers on every program run
+	// This program will create same sequence of random numbers on every program run
+
 int main(void)
 {
+	int n;
+
+	strand(time(0))
+		n = rand() - RANDMAX / 2; 
 
 	for(int i = 0; i<5; i++)
 		printf(" %d ", rand());
