fc9ec6a8c700b96d7ac7656c222b3a824cb58c42
kalinaD1924 <kaliyyeek@gmail.com>
2022-03-15 19:20:17 +0100

    c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,26 +1,19 @@
-
 #include "main.h"
 
 /**
- * main - check the code for Holberton School students.
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times.
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet_x10(void)
 {
-	print_alphabet_x10(void);
-	return (0);
-}
-{
-        int i;
-        char c;
-        for (1 - 0; i < 10; ++i)
-        {
-                for (c = 'a'; c <= 'z'; c++ )
-                {
-                    _putchar(c);
-                 }
-                _putchar('\n');
-        }
+	int i;
+	char c;
 
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
 }
