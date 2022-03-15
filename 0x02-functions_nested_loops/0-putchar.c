710e90a11d1dd372aa4b3dec794f029e8e9542e4
kalinaD1924 <kaliyyeek@gmail.com>
2022-03-15 19:38:32 +0100

    c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,13 @@
  **/
 int main(void)
 {
-
+	char ermias[] = "_putchar";
 
 	int c;
-        char c;
+
 	for (c = 0; c < 8; c++)
 	{
-		_putchar(c);
+		_putchar(ermias[c]);
 	}
 	_putchar('\n');
 	return (0);
